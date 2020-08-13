241a4e097ded86c3dad5b53e8fab7e39a5ddbf6d
Emerson <emerson_sardinha@hotmail.com>
2020-08-12 23:08:42 -0300

    Criando o profile de desenvolvimento

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -3,23 +3,32 @@ package com.nelioalves.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
 
-//auça 56
+//aula 57
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
-
-	@Bean //metodo retorna true apenas pq o metodo nao pode ser void
+	
+	//capta o valor da chave do arquivo application.properties para testar se o ambiente esta em criação ou produção
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
